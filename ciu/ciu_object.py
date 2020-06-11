965b427d7f9f580e48b0ef652558bc96b67e6575
Timotheus Kampik <timotheus.kampik@gmail.com>
2020-06-11 23:16:12 +0200

    #10 Configurability of feature interactions * One should be able to configure to only include feature interactions in a plot or textual explanation. * One should be able to configure to only include "ordinary features" in a plot or textual explanation. * By default all ordinary features and all pre-configured feature interactions should be included.

--- ciu/ciu_object.py
+++ ciu/ciu_object.py
@@ -31,12 +31,34 @@ class CiuObject:
         else:
             return 'very typical'
 
-    def plot(self, data, title):
+    @staticmethod
+    def _filter_feature_names(feature_names, interactions, include):
+        feature_names_final = []
+        for index, feature_name in enumerate(feature_names):
+            exclude_interactions = include == 'no_interactions'
+            exclude_ordinary = include == 'only_interactions'
+            exclude_feature = \
+                (exclude_interactions and feature_name in interactions) or \
+                (exclude_ordinary and feature_name not in interactions)
+            if not exclude_feature:
+                feature_names_final.append(feature_name)
+        return feature_names_final
+
+    def plot(self, data, title, include):
         plt.style.use(self.theme)
         data = data.astype(int)
         fig, ax = plt.subplots(figsize=(3, 3))
         fig.suptitle(title)
-        feature_names = self.ci.keys()
+        feature_names_prelim = self.ci.keys()
+        feature_names = self._filter_feature_names(
+            feature_names_prelim,
+            self.interactions, include
+        )
+        indices_deleted = 0
+        for index, feature_name in enumerate(feature_names_prelim):
+            if feature_name not in feature_names:
+                data = np.delete(data, index - indices_deleted)
+                indices_deleted += 1
         y_pos = np.arange(len(feature_names))
         ax.barh(y_pos, data)
         ax.set_yticks(y_pos)
@@ -52,19 +74,21 @@ class CiuObject:
         ax.spines['bottom'].set_visible(False)
         ax.spines['left'].set_visible(True)
 
-    def plot_ci(self):
+    def plot_ci(self, include='all'):
         ci = np.fromiter(self.ci.values(), dtype=float)
         ci = np.round(ci * 100)
-        self.plot(ci, 'Contextual Importance')
+        self.plot(ci, 'Contextual Importance', include)
 
-    def plot_cu(self):
+    def plot_cu(self, include='all'):
         cu = np.fromiter(self.cu.values(), dtype=float)
         cu = np.round(cu * 100)
-        self.plot(cu, 'Contextual Utility')
+        self.plot(cu, 'Contextual Utility', include)
 
-    def text_explain(self):
-        feature_names = self.ci.keys()
-
+    def text_explain(self, include='all'):
+        feature_names = self._filter_feature_names(
+            self.ci.keys(),
+            self.interactions, include
+        )
         explanation_texts = []
         for index, feature in enumerate(list(feature_names)):
             importance = self._determine_importance(self.ci[feature])
