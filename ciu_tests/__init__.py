334f6e3754a087169cc47b86ac699cbae8be490e
Vlad Apopei <vladeuvlad18@gmail.com>
2022-12-02 11:47:41 +0200

    Splitting tests
    Also adding ames housing with intermediate concepts

--- ciu_tests/__init__.py
+++ ciu_tests/__init__.py
@@ -1,3 +1,4 @@
-from .ciu_tests import get_heart_disease_rf
-from .ciu_tests import get_iris_test
-from .ciu_tests import get_boston_gbm_test
+from .heart_disease_rf import get_heart_disease_rf
+from .iris_lda import get_iris_test
+from .ames_housing_gbm import get_ames_gbm_test
+from .boston_gbm import get_boston_gbm_test
