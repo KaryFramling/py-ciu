99e33a70d5fa89fb4848f3d9695ca6b804513111
Vlad Apopei <vladeuvlad18@gmail.com>
2022-12-09 12:55:37 +0200

    Adding titanic to init

--- ciu_tests/__init__.py
+++ ciu_tests/__init__.py
@@ -2,3 +2,4 @@ from .heart_disease_rf import get_heart_disease_rf
 from .iris_lda import get_iris_test
 from .ames_housing_gbm import get_ames_gbm_test
 from .boston_gbm import get_boston_gbm_test
+from .titanic_rf import get_titanic_rf
